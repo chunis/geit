3f3299977480336b0db63339de783cd4e5cfb57e
Chunis Deng <chunchengfh@gmail.com>
2011-01-15 17:45:33 +0800

    display Chinese correctly now

--- get_latest.pl
+++ get_latest.pl
@@ -4,27 +4,40 @@ use strict;
 use warnings;
 
 use utf8;
-use LWP::Simple;
-use Data::Dumper;
+use Encode;
 
+use LWP::Simple;
 use HTML::TreeBuilder;
 
+use Data::Dumper;
 
+
+my $str;
 my $url = "http://www.itpub.net/forum-61-1.html";
 my $file = "test.html";
-# my $ret = getstore($url, $file);
-# my $ret = get($url);
+# $str = getstore($url, $file);
+# $str = get($url);
+
+open my $fh, "<", $file or die "Open $file failed: $!\n";
+my @_str = <$fh>;
+$str = join "", @_str;
+$str = decode("gb2312",$str);
 
 
-my $root = HTML::TreeBuilder->new_from_file($file);
-#$root->dump;   # print( ) a representation of the tree
-my $tt = $root->address("0.1.2.3.0.0.0.6.1.0.0.0.2.3")->content;
+# my $tree = HTML::TreeBuilder->new_from_file($file);
+my $tree = new HTML::TreeBuilder;
+$tree->parse($str);
+# print Dumper($tree), "\n";
+
+#$tree->dump;   # print( ) a representation of the tree
+# my $tt = $tree->address("0.1.2.3.0.0.0.6.1.0.0.0.2.3")->content;
 # print Dumper $tt;
 
-my $html = $root->as_HTML();
+my $html = $tree->as_HTML();
+$html = encode("gb2312", $html);
 print $html;
 
-$root->delete; # erase this tree because we're done with it
+# $tree->delete; # erase this tree because we're done with it
 
 
 
